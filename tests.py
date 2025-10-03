c0de3ac7344ccf2ff48abb2e00ef1d97262127e4
Gael Loreto <156948739+DedoDePescado03@users.noreply.github.com>
2025-10-03 03:02:09 +0000

    TCI-02

--- tests.py
+++ tests.py
@@ -6,7 +6,9 @@ def run():
     test = Library()
     test.add_book(Book("1984", "George Orwell", "1949", "ISBN001"))
     test.add_user(User("Gael", "1"))
+    test.add_user(User("Max", "2"))
     test.borrow_book("1","ISBN001")
+    test.borrow_book("2","ISBN001")
 
 if __name__ == "__main__":
     run()
