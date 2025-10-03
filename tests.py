0790ece00458df353514c2a0f2fc8dc250eabf62
Gael Loreto <156948739+DedoDePescado03@users.noreply.github.com>
2025-10-03 02:58:59 +0000

    TCI-01

--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
+from library import Library
 from book import Book
+from user import User
 
 def run():
-    test = Book("Harry Potter", "J.K Rowling", "1980", "ISBN001")
-    test.return_book()
+    test = Library()
+    test.add_book(Book("1984", "George Orwell", "1949", "ISBN001"))
+    test.add_user(User("Gael", "1"))
+    test.borrow_book("1","ISBN001")
 
 if __name__ == "__main__":
     run()
